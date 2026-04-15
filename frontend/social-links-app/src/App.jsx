bd4f02686ffddfe343d9bc599e84a678697f6e02
DanielBritoSJunior <danielbritodasilvajunior@gmail.com>
2026-04-15 00:22:34 -0300

    feat: criando e adicionando o componente footer

--- frontend/social-links-app/src/App.jsx
+++ frontend/social-links-app/src/App.jsx
@@ -1,9 +1,11 @@
 import Card from "./components/Card";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="min-h-screen bg-blue-950 text-white flex items-center justify-center px-4">
       <Card />
+      <Footer />
     </div>
   );
 }
---
+++ frontend/social-links-app/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+    return (
+        <div>
+            <p className="fixed bottom-0 mb-5 left-0 w-full text-center text-sm text-slate-50 mt-8">
+                &copy; 2026 Daniel Brito S. Junior. All rights reserved.
+            </p>
+        </div>
+    )
+}
+
+export default Footer
